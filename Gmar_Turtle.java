80e1d1b8e38df74844dc9ff6fe4c570957022ae1
Gmart46 <gavitron19@outlook.com>
2024-09-10 15:31:39 -0700

    Add files via upload

--- Gmar_Turtle.java
+++ Gmar_Turtle.java
@@ -13,8 +13,39 @@ public class Gmar_Turtle
     public static void main(String[] args)
     {
         World turtleWorld = new World();
-        Turtle maria = new Turtle(turtleWorld);
-        maria.forward(100);
+        Turtle Gmar = new Turtle(turtleWorld);
+        Gmar.penUp();
+        Gmar.moveTo(120,40);
+        Gmar.penDown();
+        Gmar.turnLeft();
+        Gmar.forward(100);
+        Gmar.turnLeft();
+        Gmar.forward(140);
+        Gmar.turnLeft();
+        Gmar.forward(100);
+        Gmar.turnLeft();
+        Gmar.forward(50);
+        Gmar.turnLeft();
+        Gmar.forward(50);
+        Gmar.penUp();
+        Gmar.turnLeft();
+        Gmar.forward(50);
+        Gmar.turnLeft();
+        Gmar.forward(80);
+        Gmar.penDown();
+        Gmar.turn(290);
+        Gmar.forward(140);
+        Gmar.turn(140);
+        Gmar.forward(140);
+        Gmar.turn(180);
+        Gmar.forward(70);
+        Gmar.turn(290);
+        Gmar.forward(48);
+        Gmar.penUp();
+        Gmar.turn(290);
+        Gmar.forward(70);
+        Gmar.turn(249);
+        Gmar.forward(110);
         // put your additional code here and delete this line!
         
         
